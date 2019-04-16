4ed097f637f3f87b39308c8f7de641ca5b14e7db
Sonia <soniadd14@msn.com>
2019-04-16 15:37:14 +0100

    .

--- src/server/server.js
+++ src/server/server.js
@@ -1,9 +1,29 @@
+const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const auth = require('@ull-esit-pl/auth');
 
 const app = express();
+//
+app.set('view engine', 'ejs');
 
+
+/*
+app.get('/login', function(req, res) {
+    res.render('../views/login');
+});
+
+app.get('/logout', function(req, res) {
+    res.render('../views/logout');
+});
+
+app.get('/register', function(req, res) {
+    res.render('../views/register');
+});
+
+//*/
+app.use(express.urlencoded({extended : true}));
+app.use(express.json());
 app.use(session({
     secret: 'verySecureSecret',
     resave: true,
@@ -22,3 +42,9 @@ app.use('/', auth({
     logoutView: 'logout',
     unauthorizedView: 'unauthorizedView',
 }));
+//
+app.get('/', function(req, res) {
+    res.render('index');
+});
+app.listen(8080);
+//
